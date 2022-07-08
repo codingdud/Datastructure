7b817ab4f7fffee621b02772f1634d9bf7d04ba4
akanoob <animeshk384@gmail.com>
2022-07-08 13:20:31 +0530

    DSA

--- binarytree/structtree.c
+++ binarytree/structtree.c
@@ -1,1 +1,21 @@
 #include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *left,*right;
+};
+
+struct node* newnode(int data){
+    struct node* temp=(struct node*)malloc(sizeof(struct node));
+    temp->data=data;
+    temp->right=temp->left=NULL;
+    return temp;
+}
+
+int main(){
+    struct node* root=newnode(10);
+    root->left=newnode(20);
+    root->right=newnode(30);
+    printf("%d",root->left->data);
+}
